24ee4bfe072a1812b6658e8a0154ecb239f7e955
Malarvili2003 <malarvili959@gmail.com>
2025-12-31 20:21:15 +0530

    Use static article data for frontend demo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
-import axios from "axios";
+import articles from "../data/articles.json";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000"
-});
+export const getArticles = async () => {
+  return articles;
+};
 
-export const fetchArticles = () => API.get("/articles");
