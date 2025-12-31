32e5a025fbd1f16bed6c3367edbdfef8bf03b360
Malarvili2003 <malarvili959@gmail.com>
2025-12-31 20:34:01 +0530

    Use static article data for frontend live demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
-import { fetchArticles } from "./services/api";
+import { getArticles } from "./services/api";
 import ArticleCard from "./components/ArticleCard";
-import "./App.css";
 
 function App() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetchArticles().then(res => setArticles(res.data));
+    getArticles().then(setArticles);
   }, []);
 
   return (
-    <div className="container">
+    <div>
       <h1>BeyondChats Articles</h1>
       {articles.map(article => (
-        <ArticleCard key={article._id} article={article} />
+        <ArticleCard key={article.id} article={article} />
       ))}
     </div>
   );
